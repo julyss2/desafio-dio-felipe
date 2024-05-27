8b981072e1799e4d9df34795ba7a91c4a951abed
julyss2 <juliasantanarosa16@gmail.com>
2024-05-27 08:37:29 -0300

    primeiro commit

---
+++ classificador.py
@@ -0,0 +1,24 @@
+# Captura do nome e experiência do herói
+nome = input("Digite o nome do herói: ")
+xp = int(input("Digite a quantidade de experiência do herói: "))
+
+# Verificação do nível do herói com base na experiência
+if xp < 1000:
+    nivel = "Ferro"
+elif 1001 <= xp <= 2000:
+    nivel = "Bronze"
+elif 2001 <= xp <= 5000:
+    nivel = "Prata"
+elif 5001 <= xp <= 7000:
+    nivel = "Ouro"
+elif 7001 <= xp <= 8000:
+    nivel = "Platina"
+elif 8001 <= xp <= 9000:
+    nivel = "Ascendente"
+elif 9001 <= xp <= 10000:
+    nivel = "Imortal"
+else:
+    nivel = "Radiante"
+
+# Exibição do resultado
+print(f"O Herói de nome {nome} está no nível de {nivel}")
